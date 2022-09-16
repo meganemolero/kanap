3774a369044b367e7591abda1ded8a9e6b890dcc
meganemolero <megane.romary@yahoo.fr>
2022-09-16 21:24:54 +0200

    différentes tentatives de résolution du problème d'affichage du numéro de commande

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -3,7 +3,10 @@ let params = new URL(window.location.href);
 let id = params.searchParams.get('orderId');
 
 /*Récupération des éléments du DOM*/
+function getOrderNumber(){
 let orderNumber = document.getElementById("orderId");
 orderNumber.innerText = id;
 
 localStorage.clear();
+};
+getOrderNumber();
