08da1ccf62eb9f9896028a6c374cb09d394b12df
meganemolero <megane.romary@yahoo.fr>
2022-09-21 11:36:36 +0200

    correction indentation

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,12 +1,13 @@
 /*Récupération des informations via url searchParams*/
-let params = new URL(window.location.href);
-let id = params.searchParams.get('orderId');
+let orderId = new URL(window.location.href).searchParams;
+let id = orderId.get('orderId');
+
 
 /*Récupération des éléments du DOM*/
 function getOrderNumber(){
-let orderNumber = document.getElementById("orderId");
-orderNumber.innerText = id;
-
-localStorage.clear();
+    console.log(id);
+    let orderNumber = document.getElementById("orderId");
+    orderNumber.innerText = id;
+    localStorage.clear();
 };
 getOrderNumber();
